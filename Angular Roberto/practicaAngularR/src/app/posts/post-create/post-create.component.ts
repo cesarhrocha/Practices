60751556e35f913b94bec2e85fa622d751455842
Roberto Vz <roberto@opalo.studio>
2018-07-11 17:22:07 -0500

    Avance del dia miercoles 11 de Julio

---
+++ Angular Roberto/practicaAngularR/src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component ({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredTitle = '';
+  enteredContent = '';
+  @Output() postCreated = new EventEmitter();
+
+  paraCrearPosts() {
+    const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+    this.postCreated.emit(post);
+  }
+
+}
