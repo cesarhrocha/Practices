3b37bffadbad709cf15dbbaccf625e644cf622c4
Roberto Vz <roberto@opalo.studio>
2018-07-12 14:37:56 -0500

    Curso completo de Angular y node juntos

--- Angular Roberto/practicaAngularR/backend/app.js
+++ Angular Roberto/practicaAngularR/backend/app.js
@@ -1,14 +1,46 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+
+
 app.use((req, res, next) => {
-  console.log('holi');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-Width, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, ELETE, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  req.setEncoding('holo');
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added yupi"
+  });
+});
+
+
+app.get("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "12345",
+      title: "Titulo 1",
+      content: "This content "
+    },
+    {
+      id: "123452",
+      title: "Titulo 2",
+      content: "This content x2 "
+    }
+  ];
+  res.status(200).json({
+    message: 'Post fetched success',
+    posts: posts
+  });
 });
 
 module.exports = app;
